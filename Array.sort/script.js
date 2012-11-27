ec818dbcf63e73fc2a7c66b9c6f70b6ff3be3e16
Thomas Giles <wthit56@googlemail.com>
2012-11-27 19:15:57 +0000

    Array.sort 0.1

---
+++ Array.sort/script.js
@@ -0,0 +1,114 @@
+﻿
+var sort = (function () {
+	// This is the default comparison method.
+	var compareStringFunction = (function () {
+		var aS, bS;
+		return function compareStringFunction(a, b) {
+
+			// Convert a and b to strings
+			aS = a.toString(); bS = b.toString();
+
+			return (
+				(aS == bS) ?	// If the strings are the same,
+					0 : 		//	return 0 (the items are next to each other, so do nothing).
+					(aS > bS) ? // If a's string is greater than b's,
+						1 : 	//	return 1 (a should be after b);
+						-1		// Else, return -1 (a should be before b).
+			);
+			// The way JavaScript compares 2 strings is to check each character one at a time,
+			//	and whichever is higher in the ASCII table wins.
+			//	(ASCII is a big table of letters, numbers and symbols,
+			//		each with a number associated with it.
+			//		These numbers are used to determine whether
+			//		one character comes before or after another character.)
+
+			// So for example, the lowercase character "a" is number 97 in the ASCII Table.
+			//	And the lowercase "b" is number 98.
+			//	Therefore, "b" is greater than "a".
+			// You can see the full ASCII Table at http://www.asciitable.com/
+
+			// There are some oddities that occur because of this method of compaison.
+			//	One obvious example is the following:
+			//	Which should come first: 10 or 5?
+			//		Obviously we would expect 5 to come first,
+			//			with 10 being the larger number.
+			//		Remember though, the numbers are converted to strings
+			//			and then compared, one character at a time.
+			//		So which comes first: 1 or 5?
+			//			1, of course.
+			//		So which of the strings comes first: "10" or "5"?
+			//			10.
+			//	This means that if you are sorting an array of numbers,
+			//		a simple .sort() won't give you accurate results.
+			//		Instead you will have to pass in your own compareFunction.
+			//	This will be a function that takes 2 arguments, and returns a number.
+			//		So if we want to compare two numbers, we can do something like this:
+			/*
+				[10, 5].sort(function(a, b){
+					return a - b;
+				}); // returns [5, 10];
+			*/
+			//	If a 0 is returned, the items are equal in greatness,
+			//		and should be next to each other in the resulting sorted array.
+			//		If a positive number is returned, b should be before a.
+			//		And if a negative number is returned, a should be before b.
+		};
+	})();
+
+	// And here's the actual sort function.
+	return function sort(array, compareFunction) {
+		// If there are less than 2 items in the array, no sorting is required.
+		if (array.length < 2) { return array; }
+
+		var i = 0;					// Index, to track the current item in loops.
+		var l = array.length - 1;	// Length, to track the last item to compare.
+									//	We will be comparing to the next item;
+									//		the last item will have no item after it compare to.
+		var r = false;				// Reverse, to keep track of whether we need to
+									//	loop through the items in reverse,
+									//	to ensure correct ordering.
+
+		// If a compareFunction has not been provided,
+		//	the default string comparison (above) is used instead.
+		compareFunction = compareFunction || compareStringFunction;
+
+		// This will loop infinitely until something inside the loop stops it.
+		while (i < l) {
+			// If there are no more items to compare to, stop the loop.
+
+			//	-1 means current item should be before the next item;
+			//		this is already the case, so we can simply continue.
+			//	0 means current item is equal to the next item;
+			//		this means they should be next to each other in the array.
+			//		Again, they are, and so we continue.
+			//	1 means the current item should be after the next item;
+			//		here we need to swap round the current item with the next one.
+			//		This can cause errors.
+			//			If the next item should be even earlier in the array,
+			//				this would never be checked for.
+			//			For this reason we need to remember to loop backward
+			//				through the array and do these checks.
+
+			// Call the compareFunction, passing in the current item and the next item in the array.
+			if (compareFunction(array[i], array[i + 1]) > 0) {
+				array.splice(i, 2, array[i + 1], array[i]);
+				r = true;
+			}
+
+			i++;
+		}
+
+		if (r) {
+			i = array.length - 1;
+			while (i > 0) {
+				if (compareFunction(array[i], array[i - 1]) < 0) {
+					array.splice(i - 1, 2, array[i], array[i - 1]);
+				}
+				i--;
+			}
+		}
+
+		return array;
+	};
+
+})();
