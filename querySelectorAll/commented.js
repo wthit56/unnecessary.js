8d973e2447c51bb161900815d4d2befdf7d4f7c3
Thomas Giles <wthit56@googlemail.com>
2013-01-29 15:01:11 +0000

    .

--- querySelectorAll/commented.js
+++ querySelectorAll/commented.js
@@ -1,6 +1,68 @@
 // https://developer.mozilla.org/en-US/docs/CSS/CSS_Reference#Selectors
-
 document.querySelectorAll = (function () {
+
+	{ // requirements
+		var failed = false;
+		if (!document.getElementsByClassName) {
+			console.error("Cannot find document.getElementsByClassName");
+			failed = true;
+		}
+		if (!document.getElementsByTagName) {
+			console.error("Cannot find document.getElementsByTagName");
+			failed = true;
+		}
+		if (!document.getElementsByTagNameNS) {
+			console.error("Cannot find document.getElementsByTagNameNS");
+			failed = true;
+		}
+
+		if (failed) { return; }
+	}
+
+	var attr = (function () {
+		var attr = {
+			name: /([a-zA-Z0-9-]+)/,
+			value: /([~\|\^\$\*])?\=(?:(["'])([\W\w]*?)\3|(\S*))/
+		};
+		attr.find = new RegExp("\\[" + attr.name.source + "(?:" + attr.value.source + ")?\\]");
+
+		attr.matchMap = {
+			"": "exact",
+			"~": "whitespace-separated",
+			"|": "hyphen-separated",
+			"^": "starts-with",
+			"$": "ends-with",
+			"*": "includes"
+		};
+		attr.replace = function (match, name, valueMatch, quote, value, unquotedValue) {
+			this.attributes.push({
+				type: attribute,
+				name: name,
+				value: (value != null) ? value : unquotedValue,
+				match: attr.matchMap[(valueMatch == null) ? "" : valueMatch]
+			});
+		};
+
+		return attr;
+	})();
+
+	var find = new RegExp(
+		attr.find.source
+	, "g");
+	console.log("find: ", find);
+
+	return function (selectors) {
+		console.group(selectors);
+
+		selectors.replace(/([\W\w]*?(?:(["'])[\W\w]*?\2))*?(?:,|$)/g, function (match) {
+			console.group(match);
+			console.groupEnd();
+		});
+		
+		console.groupEnd();
+	};
+
+
 	return function (selectors) {
 		console.group(selectors);
 		{
@@ -13,8 +75,10 @@ document.querySelectorAll = (function () {
 
 					console.log("select elements...");
 					c.replace(
-						/((?:(?:\*|([a-zA-Z0-9-]*))\|)?\*)|(?:\s*(?:(\+)|(~)|(>))\s*)|(\s+)|(?:(?:(\.)|(#))?([a-zA-Z0-9-]+(?!\|)))|(\[([a-zA-Z0-9-]+((=)|(~=)|(\|=)|(\^=)|(\$=)|(\*=))?)\])/g,
-						function handle(match, any, anyNamespace, adjacent, sibling, child, descendant, isClass, isID, name, attr, attrName, attrValue, attrIncludesWhitespace, attrIncludesHyphens, attrStartsWith, attrEndsWith, attrIncludes) {
+						/(\[([a-zA-Z0-9-]+)(([\~\|\^\$\*])=(?:(["'])([\W\w]*?)\9)|\S*)?\])|((?:(?:\*|([a-zA-Z0-9-]*))\|)?\*)|(?:\:\:?([a-z-]+))|(?:\s*(?:(\+)|(~)|(>))\s*)|(\s+)|(?:(?:(\.)|(#))?([a-zA-Z0-9-]+(?!\|)))/g,
+						function handle(match, attr, attrName, attrValue, attrIncludesWhitespace, attrIncludesHyphens, attrStartsWith, attrEndsWith, attrIncludes, any, anyNamespace, pseudo, adjacent, sibling, child, descendant, isClass, isID, name) {
+							console.log(arguments);
+
 							if (any) {
 								console.log(
 									"any element" +
@@ -27,6 +91,7 @@ document.querySelectorAll = (function () {
 									)
 								);
 							}
+							else if (pseudo) { console.log("move to pseudo (" + pseudo + ")"); }
 							else if (adjacent) { console.log("move to adjacent..."); }
 							else if (sibling) { console.log("move to sibling..."); }
 							else if (child) { console.log("move to child..."); }
@@ -37,7 +102,7 @@ document.querySelectorAll = (function () {
 								else { console.log("with tagName: ", name); }
 							}
 							else if (attr) {
-								
+
 							}
 						}
 					);
--- querySelectorAll/test.js
+++ querySelectorAll/test.js
@@ -1,1 +1,1 @@
-console.log(document.querySelectorAll("div#id.class span, .class2 > |*, *|*, ns|*, |*"));
+console.log(document.querySelectorAll("div#id.class span, .class2[attr][attr2~='value'][attr3=value] > *:first-of-type"));
