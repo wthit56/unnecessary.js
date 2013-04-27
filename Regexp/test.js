153b34d14772ad910d93786bf08fa82a7218be19
Thomas Giles <wthit56@googlemail.com>
2013-04-28 00:24:50 +0100

    Regex, first commit

---
+++ Regexp/test.js
@@ -0,0 +1,9 @@
+﻿var test = (function () {
+	var match;
+
+	return function test(regex, input) {
+		match = input.match(regex);
+
+		console.log(JSON.stringify(regex.source), ">", JSON.stringify(input), "==", null);
+	};
+})();
